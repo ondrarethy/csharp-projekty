1fe721835f67e6e88274e0dfb2f7d1f8295f5d26
ondrarethy <ondrarethy@gmail.com>
2017-11-02 13:13:56 +0100

    First prerelease
    v0.0.1-beta

---
+++ login/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MahApps.Metro.Controls;
+
+namespace WPF2test
+{
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("CMD.exe", "/C net use " + PismenoDisku.Text + ": \\\\10.2.1.2\\public " + uzivheslo.Password + " /USER:AD\\" + uzivjmeno.Text);
+            this.Close();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
